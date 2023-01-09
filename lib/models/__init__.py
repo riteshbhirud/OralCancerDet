036741afb0ccc994cb6c23f6cc251e4ebe7d13c5
riteshbhirud <83958013+riteshbhirud@users.noreply.github.com>
2023-01-08 20:37:22 -0500

    Add files via upload

---
+++ lib/models/__init__.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Copy from https://github.com/HRNet/HRNet-Image-Classification
+# Modified by Ritesh B.
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.cls_hrnet
